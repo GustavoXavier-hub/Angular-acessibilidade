5cc3e4eb4ade6af899b17714f76a60dda356486e
Gustavo Xavier <gus.guilhermexavier08@gmail.com>
2022-09-07 15:57:21 -0300

    Componente

--- Angular-acessibildade/src/app/app.module.ts
+++ Angular-acessibildade/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { YesNoButtonGroupComponent } from './shared/component/yes-no-button-group/yes-no-button-group.component';
+import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    YesNoButtonGroupModule
   ],
   providers: [],
   bootstrap: [AppComponent]
